3b717935095d9692a244816af6720e32700c84e4
rudyravelindev <rudyravelindev@gmail.com>
2024-03-07 20:16:59 -0800

    Making the player function and make the player choice case sensitive

--- scritp.js
+++ scritp.js
@@ -1,4 +1,4 @@
-// Get the computer choice funstion
+// Get the computer choice function
 const computerOption = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice () {
@@ -6,3 +6,13 @@ function getComputerChoice () {
 
 }
 getComputerChoice();
+
+// Get User choice
+let userChoice = prompt("Please enter: Rock, Paper or Sciccors: ")
+let getUserChoice =  userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLocaleLowerCase();
+
+
+function playerChoice () {
+    console.log(getUserChoice)
+}
+playerChoice();
