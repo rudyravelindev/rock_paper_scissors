38b2026135007eeafd3282420cc21ff5ee57cd36
rudyravelindev <rudyravelindev@gmail.com>
2024-03-07 19:53:59 -0800

    Creating the getComputerChoice function and I test it it works well

--- scritp.js
+++ scritp.js
@@ -1,2 +1,8 @@
-console.log("Hello Wordl!")
-console.log('It works')
+// Get the computer choice funstion
+const computerOption = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice () {
+    console.log(computerOption[(Math.floor(Math.random() * computerOption.length))]);
+
+}
+getComputerChoice();
