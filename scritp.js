dc48cbdd820aa50c9a8821778c4bf07a5b196ab5
rudyravelindev <rudyravelindev@gmail.com>
2024-03-11 23:20:23 -0700

    Creating the single round function and play a single round that show the winner

--- scritp.js
+++ scritp.js
@@ -2,17 +2,30 @@
 const computerOption = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice () {
-    console.log(computerOption[(Math.floor(Math.random() * computerOption.length))]);
+    return computerOption[Math.floor(Math.random() * computerOption.length)];
 
 }
-getComputerChoice();
 
 // Get User choice
 let userChoice = prompt("Please enter: Rock, Paper or Sciccors: ")
-let getUserChoice =  userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLocaleLowerCase();
+let getUserChoice =  userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
 
 
-function playerChoice () {
-    console.log(getUserChoice)
+//  Single Round play
+function  playRound(playerChoice, computerChoice){
+    if (playerChoice === computerChoice){
+        return "It's a tie!";
+    } else if (
+        (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
+        (playerChoice === 'Scissors' && computerChoice === 'Paper') ||
+        (playerChoice === 'Paper' && computerChoice === 'Rock')
+    ){
+        return `${playerChoice} beats ${computerChoice}: Player wins!`
+    } else{
+        return `${computerChoice} beats ${playerChoice}: Computer wins!`
+    }
+
 }
-playerChoice();
+let computerChoice = getComputerChoice();
+let result = playRound(getUserChoice, computerChoice);
+console.log(result)
