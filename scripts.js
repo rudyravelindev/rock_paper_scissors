9a7a106bbb48d1811e60e2fcb65393b5f6898675
rudyravelindev <rudyravelindev@gmail.com>
2024-05-31 00:02:48 -0700

    I keep geetin g 'computer win' It is an error

--- scripts.js
+++ scripts.js
@@ -13,3 +13,31 @@ function getHumanChoice(){
 
 }
 console.log(getHumanChoice());
+
+// score
+humanScore = 0;
+computerScore = 0;
+
+// Play a single ROUND
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === 'paper' && computerChoice === 'rock'){
+        return 'Human win'
+    }
+    else if(humanChoice === 'rock' && computerChoice === 'scissors'){
+        return 'Human win'
+    }
+    else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+        return 'Human win'
+    }
+    else if(humanChoice === computerChoice){
+        return 'It is a TIE'
+    }
+    else{
+        return 'Computer win'
+    }
+}
+const humanSelection = getHumanChoice;
+const computerSelection = getComputerChoice;
+console.log(playRound(humanSelection, computerSelection));
+
+// compare the two choice
