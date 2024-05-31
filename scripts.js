a224d492706c7eeb0ece5e28fbe195844691c780
rudyravelindev <rudyravelindev@gmail.com>
2024-05-30 20:27:28 -0700

    I get the computer choice

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
-console.log("Hello World!");
-console.log("Hello Rudy!");
+// Get the computer input: Rock, Paper or Scissors
+function getComputerChoice(){
+    const computerChoice = ['rock', 'paper', 'scissors'];
+    const computerChoose = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[computerChoose];
+}
+console.log(getComputerChoice());
+
+
+
+
+// Get the usr input
