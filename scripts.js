9ee15d883093ed6083ef37dc41cb3684e0fb6506
rudyravelindev <rudyravelindev@gmail.com>
2024-06-18 21:42:41 -0700

    Get the play a round function work well

--- scripts.js
+++ scripts.js
@@ -1,27 +1,57 @@
 // Get the computer Choice: Rock, Paper or Scissors
-function getComputerChoice(str){
+function getComputerChoice(){
     const choice = ['rock', 'paper', 'scissors']
     let computerChoice = choice[(Math.floor(Math.random() * choice.length))];
     return computerChoice;
 }
-
-console.log(getComputerChoice());
-
+// console.log(getComputerChoice());
 
 // Get the human choice
-function getHumanChoice(str){
-    let humanChoice = prompt("Please enter: 'rock', 'paper', 'scissors'")
+function getHumanChoice(){
+    let humanChoice = prompt('"Please enter: "rock", "paper", "scissors"')
     let lowerCaseHumanChoice = humanChoice.toLowerCase();
     return lowerCaseHumanChoice;
 }
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+
 
 // Creating the two variable in the global scope.
 let humanScore = 0;
 let computerScore = 0;
 
 
+
 // Single play function
 function playRound(humanChoice, computerChoice){
-    
+    if(computerChoice === 'rock' && humanChoice === 'scissors'){
+        return 'Computer WIN';   
+    }
+
+    else if(computerChoice === 'scissors' && humanChoice === 'paper'){
+        return 'computer WIN';
+    }
+    else if(computerChoice === 'paper' && humanChoice === 'rock'){
+        return 'Computer WIN';
+    }
+    else if(computerChoice === humanChoice){
+         return 'It is a TIE';
+    }
+    else{
+        return 'Human WIN';
+    }
 }
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
+
+// Display choices
+console.log('Human choice:', humanSelection);
+console.log('Computer choice:', computerSelection);
+
+// Determine result and log it
+const result = playRound(humanSelection, computerSelection);
+console.log('Result:', result);
